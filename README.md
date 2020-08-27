047180be447f47bf67c5b0aa2cbe9e49d60dcc31
Sandrine Babila Nubia <sandrine@Sandrines-MacBook-Pro.local>
2020-08-27 09:57:59 -0700

    finilising

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Fitness_Tracker
+
 Using Node.js and MongoDB
+
+This is a fitness tracking App that allow the user to track all the exercise they do.
+
+# Demo
+
+![alt text](Demo.gif)
+
+# Technologies used
+
+**Github**
+
+**Npm**
+
+**Css**
+
+**Html**
+
+**Javascrip**
+
+**Node.js**
+
+**MongoDB**
+
+# Links
+
+[deployed website](https://mighty-lake-47205.herokuapp.com/)
+
+[Github repository](https://github.com/Snubia/Fitness_Tracker.git)
+
+# For feedback, contributions or questions
+
+[Linkedin](https://www.linkedin.com/in/sandrine-nubia-975aa2172/)
+
+[Github](https://github.com/Snubia)
--- models/Workout.js
+++ models/Workout.js
@@ -1,23 +1,29 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    day: Date,
-
-    exercises:[
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Exercise"
-        }
-    ],
-    totalDuration: {
-        type: Number,
-        default: 0
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String
+      },
+      name: {
+        type: String,
+        trim: true
+      },
+      duration: Number,
+      distance: Number,
+      weight: Number,
+      reps: Number,
+      sets: Number
     }
+  ]
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
-
 
 module.exports = Workout;
